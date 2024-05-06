df0b900fa0122a8e9417a457fb80daab264d3ea0
DariaYurko <dariayurkodev@gmail.com>
2024-05-06 20:03:19 +0300

    Task-2 is finished.

--- js/test.js
+++ js/test.js
@@ -6,25 +6,73 @@
 // fooGlobal.call(); // window
 
 
-const person1 = {
-   name: "Dasha",
-   lastname: "Yurko",
-   age: 35,
+// const person1 = {
+//    name: "Dasha",
+//    lastname: "Yurko",
+//    age: 35,
+// };
+// const person2 = {
+//    name: "Yura",
+//    lastname: "Yurko",
+//    age: 49,
+// };
+// function printName() {
+//    console.log(`${this.lastname} ${this.name}`);
+// }
+// printName.call(person1);
+// printName.call(person2);
+
+
+
+const ancestor = {
+  name: "Paul",
+  age: 83,
+  surname: "Dawson",
+  heritage: "Irish",
 };
 
-const person2 = {
-   name: "Yura",
-   lastname: "Yurko",
-   age: 49,
-};
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// console.log(child.age);
+// console.log(child.heritage);
+
+/**
+ * 
+ * 
+ * ======================================= CLASS =====================================
+ */
+ 
+// class Human {
+//   constructor(humanName, humanAge) {
+//     this.name = humanName;
+//     this.age = humanAge;
+//   }
+// }
+ 
+// const person1 = new Human('Dasha', 35)
+// const person2 = new Human('Yura', 49)
+
+// console.log(person1);
+// console.log(person2);
+
+// console.log(person1.hasOwnProperty('name'));
+// console.log(person1.hasOwnProperty('age1'));
 
 
-function printName() {
-   console.log(`${this.lastname} ${this.name}`);
+const arr = ["Nanitoids", "Prolonger", "Antigravitator", "Droid"];
+
+function removeItem(item) {
+  if (arr.includes(item)) {
+    arr.splice(arr.indexOf(item), 1)
+  }
 }
 
-printName.call(person1);
-printName.call(person2);
-
-
-
+console.log(removeItem("Prolonger"));
+console.log(arr);
